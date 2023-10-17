d50a1d33c99462593e0c18f364f95773b81d88b6
Wojtek <wojciech.progr@gmail.com>
2023-10-18 01:48:42 +0200

    Simple code simplification

--- script.js
+++ script.js
@@ -6,10 +6,8 @@ const searchEngine = e => {
     const text = e.target.value.toLowerCase();
 
     li.forEach(el => {
-        const task = el.textContent
-
         /* indexOf -1 means that user input was not found in any li */
-        if (task.toLowerCase().indexOf(text) !== -1) {
+        if (el.textContent.toLowerCase().indexOf(text) !== -1) {
             el.style.display = 'block';
         } else {
             el.style.display = 'none';
